803426f016dde7bb043fa8775797474a9ff67ea1
Ahijeet <pathakabhijeet786@gmail.com>
2024-12-22 09:53:33 +0530

    added multi control, functionality, positioned the input to bottom of input. disabled all buttons by default.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,33 +4,38 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MaterialModule,CommonModule],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator';
-; 
+  ;
 
-  nums : any ;
-  temp : any = 0;
-  multemp : any = 1;
+
+ 
+  nums: any ;
+  temp: any = 0;
+  multemp: any = 1;
   state = true;
-  opeartion : any = "";
-  mem : any = 0;
+  opeartion: any = "";
+  mem: any = 0;
 
-  numb(x : any)
-  { 
-    if(this.nums == 0)
-    {
-      this.nums = " ";
+
+  numb(x: any) {
+    if(this.nums.length < 13){
+    if (this.nums == 0) {
+      this.nums = "";
     }
     this.nums = this.nums + document.getElementById(x)?.innerText;
-    console.log(this.nums);
+    }
+    else
+    {
+      this.nums  = this.nums.slice(0, 12);
+    }
   }
 
-  reset(datd : string)
-  {
+  reset(datd: string) {
     this.nums = "0";
     this.temp = 0;
     this.multemp = 1;
@@ -44,128 +49,299 @@ export class AppComponent {
     document.getElementById("memory")?.removeAttribute("disabled");
   }
 
-  resetc()
-  {
-    this.nums = this.nums.slice(0,-1);
+  resetc() {
+
+    this.nums = this.nums.slice(0, -1);
+
   }
 
-  add()
-  {
-
+  add() {
+    if (this.opeartion == "") {
       this.temp = this.temp + Number.parseFloat(this.nums);
       console.log(this.temp);
       this.nums = "";
       this.opeartion = "+";
+    }
+    else {
+      if (this.opeartion == "+") {
+        if (this.nums == "")
+          this.nums = "0";
+
+        this.temp = this.temp + Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "+";
+      }
+      else if (this.opeartion == "-") {
+        if (this.nums == "")
+          this.nums = "0";
+        if (this.temp == 0)
+          this.temp = Number.parseFloat(this.nums) - 0;
+        else
+          this.temp = this.temp - Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "+";
+      }
+      else if (this.opeartion == "*") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        this.multemp = this.multemp * Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "+";
+      }
+      else if (this.opeartion == "/") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        this.multemp = this.multemp / Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "+";
+      }
+    }
 
   }
 
-  subtract()
-  {
-      if(this.temp == 0)
-      this.temp = Number.parseFloat(this.nums)-0;
+  subtract() {
+    if (this.opeartion == "") {
+      if (this.temp == 0)
+        this.temp = Number.parseFloat(this.nums) - 0;
       else
-      this.temp = this.temp - Number.parseFloat(this.nums);
+        this.temp = this.temp - Number.parseFloat(this.nums);
       console.log(this.temp);
       this.nums = "";
       this.opeartion = "-";
+    }
+    else {
+      if (this.opeartion == "+") {
+        if (this.nums == "")
+          this.nums = "0";
 
+        this.temp = this.temp + Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "-";
+      }
+      else if (this.opeartion == "-") {
+        if (this.nums == "")
+          this.nums = "0";
+        if (this.temp == 0)
+          this.temp = Number.parseFloat(this.nums) - 0;
+        else
+          this.temp = this.temp - Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "-";
+      }
+      else if (this.opeartion == "*") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        this.multemp = this.multemp * Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "-";
+      }
+      else if (this.opeartion == "/") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        this.multemp = this.multemp / Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "-";
+      }
+    }
   }
 
-  divide()
-  {   
-      if(this.multemp == 1)
-      this.multemp = Number.parseFloat(this.nums)/this.multemp;
+  divide() {
+    if (this.opeartion == "") {
+      if (this.multemp == 1)
+        this.multemp = Number.parseFloat(this.nums) / this.multemp;
       else
-      this.multemp = this.multemp / Number.parseFloat(this.nums);
+        this.multemp = this.multemp / Number.parseFloat(this.nums);
       console.log(this.multemp);
       this.nums = "";
       this.opeartion = "/";
+    }
+    else {
+      if (this.opeartion == "+") {
+        if (this.nums == "")
+          this.nums = "0";
+
+        this.temp = this.temp + Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "/";
+        this.multemp = this.temp;
+      }
+      else if (this.opeartion == "-") {
+        if (this.nums == "")
+          this.nums = "0";
+        if (this.temp == 0)
+          this.temp = Number.parseFloat(this.nums) - 0;
+        else
+          this.temp = this.temp - Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "/";
+        this.multemp = this.temp;
+      }
+      else if (this.opeartion == "*") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        this.multemp = this.multemp * Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.multemp);
+        this.nums = "";
+        this.opeartion = "/";
+      }
+      else if (this.opeartion == "/") {
+        if (this.nums == "")
+          this.nums = "1";
+
+        if (this.multemp == 1)
+          this.multemp = Number.parseFloat(this.nums) / this.multemp;
+        else
+          this.multemp = this.multemp / Number.parseFloat(this.nums);
+        this.temp = this.multemp;
+        console.log(this.multemp);
+        this.nums = "";
+        this.opeartion = "/";
+      }
+
+    }
+
   }
 
-  multiply()
-  {
+  multiply() {
+
+    if (this.opeartion == "") {
       this.multemp = this.multemp * Number.parseFloat(this.nums);
       console.log(this.multemp);
       this.nums = "";
       this.opeartion = "*"
-  }
-  equals()
-  { 
-    
-    if(this.opeartion == "+")
-      {
-        if(this.nums == "")
-          this.nums="0";
-        
+    }
+
+    else {
+      if (this.opeartion == "+") {
+        if (this.nums == "")
+          this.nums = "0";
+
         this.temp = this.temp + Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "*"
+        this.multemp = this.temp;
       }
-    else if(this.opeartion == "-")
-      {
-        this.temp = this.temp - Number.parseFloat(this.nums);
+      else if (this.opeartion == "-") {
+        if (this.nums == "")
+          this.nums = "0";
+
+         if (this.temp == 0)
+          this.temp = Number.parseFloat(this.nums) - 0;
+        else
+          this.temp = this.temp - Number.parseFloat(this.nums);
+        console.log(this.temp);
+        this.nums = "";
+        this.opeartion = "*"
+        this.multemp = this.temp;
       }
-    else if(this.opeartion == "*")
-      {
-        if(this.nums == "")
-          this.nums="1";
-        
+      else if (this.opeartion == "*") {
+        if (this.nums == "")
+          this.nums = "1";
+
         this.multemp = this.multemp * Number.parseFloat(this.nums);
         this.temp = this.multemp;
+        console.log(this.multemp);
+        this.nums = "";
+        this.opeartion = "*"
       }
-    else if(this.opeartion == "/")
-      { 
-        if(this.nums == "")
-          this.nums="1";
+      else if (this.opeartion == "/") {
+        if (this.nums == "")
+          this.nums = "1";
 
         this.multemp = this.multemp / Number.parseFloat(this.nums);
-        this.temp = this.multemp;
+        this.temp = this.multemp; console.log(this.multemp);
+        this.nums = "";
+        this.opeartion = "*"
       }
+    }
+  }
+  equals() {
+
+    if (this.opeartion == "+") {
+      if (this.nums == "")
+        this.nums = "0";
+
+      this.temp = this.temp + Number.parseFloat(this.nums);
+    }
+    else if (this.opeartion == "-") {
+      this.temp = this.temp - Number.parseFloat(this.nums);
+    }
+    else if (this.opeartion == "*") {
+      if (this.nums == "")
+        this.nums = "1";
+
+      this.multemp = this.multemp * Number.parseFloat(this.nums);
+      this.temp = this.multemp;
+    }
+    else if (this.opeartion == "/") {
+      if (this.nums == "")
+        this.nums = "1";
+
+      this.multemp = this.multemp / Number.parseFloat(this.nums);
+      this.temp = this.multemp;
+    }
     this.nums = (this.temp);
     this.temp = 0;
     this.multemp = 1;
   }
 
-  sqrt()
-  {
-    if(!(this.nums == 0) )
-    this.nums = Math.sqrt(Number.parseFloat(this.nums));
+  sqrt() {
+    if (!(this.nums == 0))
+      this.nums = Math.sqrt(Number.parseFloat(this.nums));
   }
 
-  percent()
-  { 
-    if(!(this.nums == 0) )
-    this.nums = Number.parseFloat(this.nums)/100;
+  percent() {
+    if (!(this.nums == 0))
+      this.nums = Number.parseFloat(this.nums) / 100;
   }
 
-  off(data : string)
-  {
+  off(data: string) {
     this.nums = "";
     this.state = false;
-    document.getElementById(data)?.setAttribute("disabled","true");
+    document.getElementById(data)?.setAttribute("disabled", "true");
     const elements = document.getElementsByClassName("number");
     Array.from(elements).forEach((element) => {
       element.setAttribute("disabled", "true");
     });
-    document.getElementById("clear")?.setAttribute("disabled","true");
-    document.getElementById("equals")?.setAttribute("disabled","true");
-    document.getElementById("memory")?.setAttribute("disabled","true");
-}
+    document.getElementById("clear")?.setAttribute("disabled", "true");
+    document.getElementById("equals")?.setAttribute("disabled", "true");
+    document.getElementById("memory")?.setAttribute("disabled", "true");
+  }
 
-memory()
-{
-  this.nums = this.mem;
-}
+  memory() {
+    this.nums = this.mem;
+  }
 
-memp()
-{
-  this.mem = this.mem + this.nums;
-  console.log(this.mem);
-}
+  memp() {
+    this.mem = this.mem + this.nums;
+    console.log(this.mem);
+  }
 
-mems()
-{
-  if(this.mem == 0)
-  this.mem = this.nums-this.mem;
-  else
-  this.mem = this.mem - this.nums;
-}
+  mems() {
+    if (this.mem == 0)
+      this.mem = this.nums - this.mem;
+    else
+      this.mem = this.mem - this.nums;
+  }
 }
